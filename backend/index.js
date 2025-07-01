8276f12aa12501dfae1af60523f0f8898d638c10
Lokesh Pawar <lokeshpawar921@gmail.com>
2025-07-02 00:05:46 +0530

    did some deployment thing from chat app vdo

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const { connectDB } = require("./util/db");
 const mainRouter = require("./routes/index"); //check here if route is correct
 const cors = require("cors");
+const path = require("path");
 
 const port = 5001;
 const app = express();
 
+const __dirname = path.resolve();
 app.use(cors());
 app.use(express.json());
 
@@ -13,6 +15,13 @@ app.use(express.json());
 
 app.use("/api/v1", mainRouter);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "./frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./frontend", "dist", "index.html"));
+  });
+}
 // not neeeded
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
